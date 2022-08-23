a0cbb3c1155e541ff1439531c5baf1e164f8067f
elija33 <amponsaeli@gmail.com>
2022-08-22 22:20:35 -0400

    end

--- CircularLInkedList/InsertNodeCircularLink/InsertNodeEndCircular/EfficInsertNodeAtEnd.java
+++ CircularLInkedList/InsertNodeCircularLink/InsertNodeEndCircular/EfficInsertNodeAtEnd.java
@@ -1,5 +1,7 @@
 package CircularLInkedList.InsertNodeCircularLink.InsertNodeEndCircular;
 
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
 public class EfficInsertNodeAtEnd {
     Node head;
 
@@ -13,6 +15,45 @@ public class EfficInsertNodeAtEnd {
         }
     }
     public static void main(String[] args) {
-        
+        Node head = new Node(20);
+        head.next = new Node(18);
+        head.next.next = new Node(16);
+        head.next.next.next = new Node(14);
+        head.next.next.next.next = new Node(12);
+        head.next.next.next.next.next = new Node(10);
+        head.next.next.next.next.next.next = new Node(8);
+        head.next.next.next.next.next.next.next = new Node(6);
+        head.next.next.next.next.next.next.next.next = new Node(4);
+        head.next.next.next.next.next.next.next.next.next = new Node(2);
+        printlist(head);
+        head = insertNodeEnd(head, 1);
+    }
+
+    public static void printlist(Node head){
+        if(head == null)
+        return;
+        Node list = head;
+        do{
+            System.out.print(list.data+ "->");
+            list = list.next;
+        }
+        while(list != head);
+    }
+
+    //Inserting a nodd at the end 
+    public static Node insertNodeEnd(Node head, int givenumber){
+        Node temp = new Node(givenumber);
+            if(head == null){
+                temp.next = temp;
+                return temp;
+            }  
+            else{
+                temp.next = head.next;
+                head.next = temp;
+                int ta = temp.data;
+                head.data = temp.data;
+                temp.data = ta;
+                return temp;
+            }
     }
 }
--- CircularLInkedList/InsertNodeCircularLink/InsertNodeEndCircular/NaiveInsertatEnd.java
+++ CircularLInkedList/InsertNodeCircularLink/InsertNodeEndCircular/NaiveInsertatEnd.java
@@ -1,5 +1,52 @@
 package CircularLInkedList.InsertNodeCircularLink.InsertNodeEndCircular;
 
 public class NaiveInsertatEnd {
+    Node head;
+
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int k){
+            data = k;
+            next = null;
+        }
+    }
+    public static void main(String[] args) {
+        Node head = new Node(11);
+        head.next = new Node(9);
+        head.next.next = new Node(7);
+        head.next.next.next = new Node(5);
+        head.next.next.next.next = new Node(3);
+        head.next.next.next.next.next = head;
+        head = insertNodeAtEnd(head, 1);
+        printlist(head);
+    }
+
+    public static void printlist(Node head){
+        if(head == null)
+        return;
+        Node curr = head;
+        do{
+            System.out.print(curr.data+ "->");
+            curr = curr.next;
+        }
+        while(curr != head);
+    }
     
+    public static Node insertNodeAtEnd(Node head, int givenumber){
+        Node temp = new Node(givenumber);
+        if(head == null){
+            temp.next = temp;
+            return temp;
+        }
+        else{
+            Node curr = head;
+            while(curr.next != head)
+            curr = curr.next;
+            curr.next = temp;
+            temp.next = head;
+            return head;
+        }
+    }
 }
